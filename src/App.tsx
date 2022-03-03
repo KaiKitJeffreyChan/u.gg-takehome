5a1dea464ff95a2c846298c6e352a29058097a46
jeffreychan <kkjeffreychan@gmail.com>
2022-03-03 14:20:59 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Banana
         </p>
         <a
           className="App-link"
